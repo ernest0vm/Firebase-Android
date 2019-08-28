43b4de3baae35c240db19b14bbb07b745c620567
Ernesto Valdez <ernestovm07@gmail.com>
2019-08-28 14:04:14 -0500

    Event listener for Firebase changes

--- app/src/main/java/com/ernestovaldez/keyboardshortcuts/MainActivity.java
+++ app/src/main/java/com/ernestovaldez/keyboardshortcuts/MainActivity.java
@@ -6,9 +6,13 @@ import com.ernestovaldez.keyboardshortcuts.DTO.Shortcut;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -58,6 +62,25 @@ public class MainActivity extends MainBaseActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference reference = firebaseDatabase.getReference();
+        reference.child("root").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+                for (DataSnapshot child : children){
+                    Shortcut shortcut = child.getValue(Shortcut.class);
+                    Toast.makeText(getApplicationContext(), shortcut.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
